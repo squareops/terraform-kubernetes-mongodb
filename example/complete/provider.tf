3739b02d81fb73839b49c88c2347d7ac84138867
RohitSquareops <rohit.kumar@squareops.com>
2023-04-21 18:26:07 +0530

    updated release

--- example/complete/provider.tf
+++ example/complete/provider.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 
 data "aws_eks_cluster" "cluster" {
-  name = ""
+  name = "test-squareops"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = ""
+  name = "test-squareops"
 }
 
 
